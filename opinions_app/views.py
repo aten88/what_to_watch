29400c64a7a8ca5b07bcf9b9cf942a9c9ab2a030
aten88 <virtuoso_ten@mail.ru>
2023-12-26 15:57:50 +0300

    Сделал рефакторинг проекта

---
+++ opinions_app/views.py
@@ -0,0 +1,41 @@
+from random import randrange
+
+from opinions_app.models import Opinion
+from opinions_app.forms import OpinionForm
+from flask import render_template, redirect, url_for, flash, abort
+from . import app, db
+
+
+@app.route('/')
+def index_view():
+    quantity = Opinion.query.count()
+    if not quantity:
+        abort(404)
+    offset_value = randrange(quantity)
+    opinion = Opinion.query.offset(offset_value).first()
+    return render_template('opinion.html', opinion=opinion)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_opinion_view():
+    form = OpinionForm()
+    if form.validate_on_submit():
+        text = form.text.data
+        if Opinion.query.filter_by(text=text).first() is not None:
+            flash('Такое мнение уже было оставлено ранее!')
+            return render_template('add_opinion.html', form=form)
+        opinion = Opinion(
+            title=form.title.data,
+            text=form.text.data,
+            source=form.source.data
+        )
+        db.session.add(opinion)
+        db.session.commit()
+        return redirect(url_for('opinion_view', id=opinion.id))
+    return render_template('add_opinion.html', form=form)
+
+
+@app.route('/opinions/<int:id>')
+def opinion_view(id):
+    opinion = Opinion.query.get_or_404(id)
+    return render_template('opinion.html', opinion=opinion)
