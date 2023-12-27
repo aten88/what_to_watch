5dd0b0f305712375a499501a5bc670e4f97052eb
aten88 <virtuoso_ten@mail.ru>
2023-12-27 12:10:04 +0300

    Сделал API получение, обновление и удаление по id

--- opinions_app/models.py
+++ opinions_app/models.py
@@ -10,3 +10,13 @@ class Opinion(db.Model):
     source = db.Column(db.String(256))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     added_by = db.Column(db.String(64))
+
+    def to_dict(opinion):
+        return dict(
+            id=opinion.id,
+            title=opinion.title,
+            text=opinion.text,
+            source=opinion.source,
+            timestamp=opinion.timestamp,
+            added_by=opinion.added_by
+        )
